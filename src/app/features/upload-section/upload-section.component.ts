66d9eaa8573018058702150f15dc775b94262a05
meghasharma-dits <megha.sharma@ditstek.com>
2025-06-18 10:12:30 +0530

    Id card seperation changes

--- src/app/features/upload-section/upload-section.component.ts
+++ src/app/features/upload-section/upload-section.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { TabsModule } from 'primeng/tabs';
-import { InvoiceComponent } from '../invoice/invoice.component';
-import { MailLinkingComponent } from '../mail-linking/mail-linking.component';
 import { IdCardsComponent } from '../id-cards/id-cards.component';
-// import { AiVoiceProcessingComponent } from "../ai-voice-processing/ai-voice-processing.component";
 
 @Component({
   selector: 'app-upload-section',
   imports: [
     TabsModule,
-    InvoiceComponent,
-    MailLinkingComponent,
     IdCardsComponent,
-    // AiVoiceProcessingComponent
 ],
   templateUrl: './upload-section.component.html',
   styleUrl: './upload-section.component.scss'
