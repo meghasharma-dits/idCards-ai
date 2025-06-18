ae22a065828ddaa90b89bd7886a2f5076c04e832
meghasharma-dits <megha.sharma@ditstek.com>
2025-06-18 11:59:14 +0530

    redirection changes

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { StorageKeys } from '../core/enums/storage-keys.enum';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-authentication',
@@ -9,21 +10,31 @@ import { StorageKeys } from '../core/enums/storage-keys.enum';
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.scss'
 })
-export class AuthenticationComponent implements AfterViewInit{
+export class AuthenticationComponent implements AfterViewInit {
   token: string | null = null;
+  redirectUrl: string = environment.redirectUrl;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute) { }
   ngAfterViewInit(): void {
     setTimeout(() => this._openLoginModal(), 100);
   }
 
   private _openLoginModal() {
     this.route.queryParams.subscribe(async params => {
-      this.token = params['token']; 
-      if(this.token) {
+      this.token = params['token'];
+      if (this.token) {
         sessionStorage.setItem(StorageKeys.token, this.token);
         this.router.navigate(['/invoice']);
+      } else {
+        setTimeout(() => {
+          this.redirectToListingPage();
+        }, 3000);
       }
     });
+  }
+
+  redirectToListingPage() {
+    sessionStorage.clear();
+    window.location.href = this.redirectUrl; //actual url of the application
   }
 }
